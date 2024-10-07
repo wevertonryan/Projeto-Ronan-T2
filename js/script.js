4ca8e035c323ea0adced38e1542c196fff363e2f
wevertonryan <wevertonryan205@gmail.com>
2024-10-07 19:14:52 -0300

    commit paginas

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-print("oi")
+let tema = ""
+const temaEscuro = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    
+    if (temaEscuro) {
+      tema = "img/icone/Logo_icon_white.png"
+    } else {
+      tema = "img/icone/Logo_icon.png"
+    }
+
+    document.getElementById("favicon").setAttribute("href", tema);
